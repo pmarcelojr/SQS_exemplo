85434d7e2047fb27304fd5f6bf27f7893ad099aa
pmarcelojr <marcelosantostecnologia@gmail.com>
2022-01-11 16:14:10 -0300

    criando fila SQS com Fornecedor e Consumidor

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumidor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Consumidor !");
+
+            var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.us-east-1.amazonaws.com/341336031834/teste"
+            };
+
+            while (true)
+            {
+                var response = await client.ReceiveMessageAsync(request);
+
+                foreach (var mensagem in response.Messages)
+                {
+                    // Processa a mensagem
+                    Console.WriteLine(mensagem.Body);
+                    // Deleta a mensagem para não voltar para a fila o tempo todo
+                    await client.DeleteMessageAsync("https://sqs.us-east-1.amazonaws.com/341336031834/teste", mensagem.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Amazon;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Fornecedor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Fornecedor !");
+
+            var client = new AmazonSQSClient(RegionEndpoint.USEast1);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.us-east-1.amazonaws.com/341336031834/teste",
+                MessageBody = "teste 123"
+            };
+
+            try
+            {
+                await client.SendMessageAsync(request);
+                Console.WriteLine("Mensagem Enviada");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+    }
+}
